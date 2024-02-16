13294286abec88a996a0510d37e157c19f1111d9
601-16 <601-16@DESKTOP-J2TK4C1>
2024-02-16 17:41:35 +0900

    재업로드. 로또 구현 다시 하기

--- jsp_action/src/edu/sejong/ex/Lotto.java
+++ jsp_action/src/edu/sejong/ex/Lotto.java
@@ -1,10 +1,15 @@
 package edu.sejong.ex;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Lotto {
 	private String lottoStr = "";
 	private int[] lottos = new int[6];
+	private Set<Integer> lottos2 = new HashSet<Integer>();
 	
 	public Lotto() {
+		// for문으로만 해결해보기
 		for(int i = 0; i < 6; i++) {
 			int lotto = (int)(Math.random()*45 + 1);
 			lottos[i] = lotto;
@@ -16,9 +21,22 @@ public class Lotto {
 			}
 		}
 		
-		for(int i = 0; i < 6 ; i++) {
-			lottoStr += (lottos[i] + " ");
+		// set으로 해결해보기
+		while(true) {
+			int lotto = (int)(Math.random()*45 + 1);
+			lottos2.add(lotto);
+			if(lottos2.size() == 6) {
+				break;
+			}			
 		}
+		
+		// for문 사용 시 str 설정
+//		for(int i = 0; i < 6 ; i++) {
+//			lottoStr += (lottos[i] + " ");
+//		}
+		
+		// set 사용 시 str 설정
+		lottoStr = lottos2.toString();
 	}
 
 	public String getLottoStr() {
